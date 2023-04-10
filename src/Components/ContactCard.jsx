a2ddf4eae794ddba0a388d54f4a773969f1bc3a4
bispo-daniel <dbispo.work@gmail.com>
2023-04-10 02:09:08 -0300

    Update ContactCard.jsx

--- src/Components/ContactCard.jsx
+++ src/Components/ContactCard.jsx
@@ -4,7 +4,7 @@ function ContactCard(props) {
             <div className="card-body">
                 <h5 className="card-title">{props.title}</h5>
                 <p className="card-text">{props.text}</p>
-                <a href="#" className="btn btn-primary">{props.btnText}</a>
+                <a href="https://google.com" className="btn btn-primary">{props.btnText}</a>
             </div>
         </div>
     )
